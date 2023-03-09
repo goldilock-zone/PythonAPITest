6b59dd64d13a5f540a185bca80dbfcbe7a0e06b8
goldilock-zone <sarbajitghosh2002@gmail.com>
2023-03-09 19:51:25 +0530

    changes and making notes md file

--- main.py
+++ main.py
@@ -1,9 +1,32 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
 @app.get('/') #this is a route or a path operation
-async def root():
-    return {"message": "Hello World"}
+#get - this is the type of request that will be sent as an HTTP method
+#The '/' is an indication to thge usage pf the root path of the website or the api dependign on the user case
+def root():
+    
+    return {"message": "Welcome to my API!!!"} # FastAPI automatically converts dictionary outputs to JSON
+
+@app.get('/posts')
+def get_posts():
+    #logic for retrieveing posts
+    return{"data": "this is your posts"}
 
 # to start the server weuse the uvicorn library
+
+#NOTE: Fast API goes down the list of functions seuqentially, unless we specific asynchornicity, and refereneces teh first match
+
+#NOTE: while running the server if we use:
+#uvicorn main:app --reload
+#the --reload is meant to run a live server - which keeps track of code changes continuously
+#Do not use a --reload outside development environment
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...) ): #body returns a dict
+    #In postman, we send the data as part of the "body" of the request
+    print(payload)
+    return {"new_post": f"Title: {payload['title']} Content: {payload['content']}"}
+
---
+++ notes.md
@@ -0,0 +1,15 @@
+# Day 1
+
+1. Get used to making get and post requests on postman
+2. We use FASTAPI
+3. @app.get(), is the decorator we use to make paths in our app
+4. Always use a virtual enviorment while developing apps
+5. app=FastAPI() is the way you intialize apps on FastAPI
+6. Uvicorn main:app --reload is the way that we run the app in dev mode, where we dont have to restart the server everytime we make a change to our code
+
+# Day 2
+
+1. It is important to make a schema for the data that we use as a back and forth data format between the backend and the front end
+2. Use pydantic to make schemas
+
+# Day 3
