3364d65ddf6024717a83d012615de8aafc54faef
goldilock-zone <sarbajitghosh2002@gmail.com>
2023-03-12 22:07:11 +0530

    changes to notes md, along with main.py ts: 1hr 43

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel
+from typing import Optional
+import random
 
 app = FastAPI()
+
+class Post(BaseModel):
+    # pydantic classes are used for in-built auto validation of data
+    title: str
+    content: str
+    published: bool = True #this is a default value if the user does not defien a value for us
+    rating: Optional[int] = None
+
+#We need to make our API CRUD compliant
 
 @app.get('/') #this is a route or a path operation
 #get - this is the type of request that will be sent as an HTTP method
@@ -10,10 +22,16 @@ def root():
     
     return {"message": "Welcome to my API!!!"} # FastAPI automatically converts dictionary outputs to JSON
 
-@app.get('/posts')
-def get_posts():
-    #logic for retrieveing posts
-    return{"data": "this is your posts"}
+#Just testing the routing of FASTAPI with this function
+
+
+#-----------------------------------------------------------
+# @app.get('/posts')
+# def get_posts():
+#     #logic for retrieveing posts
+#     return{"data": "this is your posts"}
+
+#-----------------------------------------------------------
 
 # to start the server weuse the uvicorn library
 
@@ -24,9 +42,40 @@ def get_posts():
 #the --reload is meant to run a live server - which keeps track of code changes continuously
 #Do not use a --reload outside development environment
 
-@app.post("/createposts")
-def create_posts(payload: dict = Body(...) ): #body returns a dict
-    #In postman, we send the data as part of the "body" of the request
-    print(payload)
-    return {"new_post": f"Title: {payload['title']} Content: {payload['content']}"}
+#-----------------------------------------------------------
+#NOTE:first implementation of the create_posts function
 
+# @app.post("/createposts")
+# def create_posts(payload: dict = Body(...) ): #body returns a dict
+#     #In postman, we send the data as part of the "body" of the request
+#     print(payload)
+#     return {"new_post": f"Title: {payload['title']} Content: {payload['content']}"}
+
+# for a post we want:
+#title: str, content: str ( category, published: Bool)
+
+#-----------------------------------------------------------
+#NOTE: second implementation of the create_posts function\
+
+
+my_posts = [
+    {"title": "Title1", "content": "Content1", "id": 1},
+    {"title": "Title2", "content": "Content2", "id": 2},
+    ]
+
+@app.get('/posts')
+def get_posts(): #based on the naming convention, posts means all posts
+    return{"data": my_posts} # the good thing about fast api is that if we send data in the form of a list, it automatically serializes it for us in JSON
+
+@app.post("/posts")
+def create_posts(post: Post):
+    post_dict = post.dict()
+    post_dict["id"] = random.randrange(0,1000000)
+    print(post_dict)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get('/posts/{id}') #technical term for the id feild is called a path parameter
+def get_post(id): #the parameters of the function have access to the path parameters in the decorator above
+    print(id)
+    return {"post_detail" : f"Here is post {id}"}
--- notes.md
+++ notes.md
@@ -6,10 +6,18 @@
 4. Always use a virtual enviorment while developing apps
 5. app=FastAPI() is the way you intialize apps on FastAPI
 6. Uvicorn main:app --reload is the way that we run the app in dev mode, where we dont have to restart the server everytime we make a change to our code
+7. Here the doc link for fast api: https://fastapi.tiangolo.com/
 
 # Day 2
 
 1. It is important to make a schema for the data that we use as a back and forth data format between the backend and the front end
-2. Use pydantic to make schemas
+2. Use pydantic to make schemas. Here's the doc link to pydantic's website: https://docs.pydantic.dev/
+3. pydantic gives a lot of inbuilt data validation for the JSON going back abnd forth between the front end and the back end of the system. To do so we create a pydantic model class that extends the BASeModel class of pydantic
+4. We need to make CRUD compliant app: Create (POST), Read (GET) (either something specific, or all the data which is a non-specific request)), Update(PUT - pass every feild/PATCH - pass only the feild that needs to be changed) and Delete (DELETE)
+5. Try exploring the typing library of python
+6. The good thing about fast api is that if we send data in the form of a list, it automatically serializes it for us in JSON (JSON also has the concept of an array)
+7. Realize: whatever your function returns is the  data that you send back to the user
+8. Naming convention for functions that are GET requests: use plural for requests that involve retrieving all objects from an object type, and use singular when we reference the id to get a specific object (eg: get_posts() for all posts, vs get_post for a single post)
+9. The path parameter that is passed in the api query is normally to refernce a particular object from the database (example, the id for a post)
 
 # Day 3
